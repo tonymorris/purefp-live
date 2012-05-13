90b11c414ceb4144123e6ce438136bf78866bbfb
Tony Morris <tmorris@tmorris.net>
2012-05-14 08:21:09 +1000

    iteratee

--- src/docbook/Iteratee.scala
+++ src/docbook/Iteratee.scala
@@ -9,3 +9,26 @@ case class Done[E, A](a: A, r: Input[E])
     extends Iteratee[E, A]
 case class Cont[E, A](k: Input[E] => Iteratee[E, A])
     extends Iteratee[E, A]
+
+object Iteratee {
+  def peek[E]: Iteratee[E, Option[E]] = {
+    def step(s: Input[E]): Iteratee[E, Option[E]] =
+      s match {
+        case Empty() => Cont(step)
+        case Eof()   => Done(None, Eof())
+        case El(k)   => Done(Some(k), Eof())
+      }
+    Cont(step)
+  }
+
+  def drop[E](n: Int): Iteratee[E, Unit] = {
+    def step(s: Input[E]): Iteratee[E, Unit] =
+      s match {
+        case Empty() => Cont(step)
+        case Eof()   => Done((), Eof())
+        case El(_)   => drop(n - 1)
+      }
+    if (n == 0) Done((), Empty())
+    else Cont(step)
+  }
+}
